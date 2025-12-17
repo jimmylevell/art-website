4cf8ad33685968ea5809c77c481272121255f327
James Levell <jimmy.levell@outlook.com>
2025-12-17 08:52:15 +0100

    Fix: added anonymosu route

--- backend/src/api/art-piece/routes/art-piece.ts
+++ backend/src/api/art-piece/routes/art-piece.ts
@@ -4,4 +4,17 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::art-piece.art-piece');
+export default factories.createCoreRouter('api::art-piece.art-piece', {
+    config: {
+        find: {
+            auth: false,
+            policies: [],
+            middlewares: [],
+        },
+        findOne: {
+            auth: false,
+            policies: [],
+            middlewares: [],
+        },
+    },
+});
